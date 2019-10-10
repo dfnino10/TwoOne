9a616b240da8b27d0e4530789a06afb46a44a57a
Andrea <ac.beltrans@uniandes.edu.co>
2019-10-09 22:11:22 -0500

    adding icon

--- imports/ui/NavBar.jsx
+++ imports/ui/NavBar.jsx
@@ -8,8 +8,8 @@ const NavBar = () => {
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
+        <a className="nav-link" href=""><img src={"https://github.com/acbeltrans/acbeltrans.github.io/blob/master/images/Icono.png?raw=true"} width="50" height="50" title="Brand image" alt="Navegation link with the image of our app"/></a>
         <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-          <a className="nav-link" href=""><img src={"https://github.com/acbeltrans/acbeltrans.github.io/blob/master/images/icon.png?raw=true"} width="50" height="50" title="Brand image" alt="Navegation link with the image of our app"/></a>
           <div className="navbar-nav mr-auto mt-2 mt-lg-0">
           </div>
           <AccountsUIWrapper></AccountsUIWrapper>
