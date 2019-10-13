b7247ae5d7921ede20b709da8372665dc965a712
Daniel Ramirez <da.ramirez14@uniandes.edu.co>
2019-10-13 13:15:14 -0500

    Adding Calendar

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -6,37 +6,35 @@ import { withTracker } from "meteor/react-meteor-data";
 import Home from "./Home.jsx";
 import Item from "./Item.jsx";
 import Navbar from "./NavBar";
-import './App.css';
+import Calendar from "./Calendar";
+import "./App.css";
 
-const App = (props) => {
-  if (props.currentUser)
-  {
+const App = props => {
+  if (props.currentUser) {
     return (
       <div>
         <Navbar></Navbar>
         <Router>
-          <Route exact path="/" component={Home}/>
-          <Route exact path="/Shopping-List" component={Item}/>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/Shopping-List" component={Item} />
+          <Route exact path="/Calendar" component={Calendar}></Route>
         </Router>
       </div>
     );
-  }
-  else {
+  } else {
     return (
       <div>
         <Navbar></Navbar>
         <Router>
-          <Route exact path="/" component={Home}/>
+          <Route exact path="/" component={Home} />
         </Router>
       </div>
     );
   }
-
 };
 
 export default withTracker(() => {
-
   return {
-    currentUser: Meteor.user(),
+    currentUser: Meteor.user()
   };
 })(App);
